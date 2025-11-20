4ab037c7fbeabbddb65d9921e13df9509e90d627
Raj Tejaswee <rajtejaswee02@gmail.com>
2025-11-20 23:49:38 +0530

    DSA

--- Arrays/linearSearch.cpp
+++ Arrays/linearSearch.cpp
@@ -7,4 +7,16 @@ int main(){
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
+    int target;
+    cin >> target;
+    for (int i = 0; i < n; i++) {
+        if(arr[i] == target){
+            cout << target << "is present at index" << endl;
+            break;
+        }
+        else {
+            cout << target << "is not present" << endl;
+        }
+    }
+    return 0;
 }
